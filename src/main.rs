96c23441cd787667f62b7a059280727f5445f030
Jason Dagit <dagitj@gmail.com>
2021-06-28 17:09:42 -0700

    It works!

--- src/main.rs
+++ src/main.rs
@@ -18,29 +18,35 @@ fn main() {
         let c = call_cc(|exit1| -> Cont<'_, char, char> {
             let p = Cont::pure(1);
             let e = p.bind(move |_: i32| exit1('a'));
-            e
+            let d: Cont<'_, char, char> = e.clone().bind(|_: char| unimplemented!());
+            d
         });
         println!("c = {:#?}", c.eval_cont());
     }
-    //foo()
+    foo()
 }
 
 #[derive(Clone)]
 pub struct Cont<'a, R, A> {
     //run: (a -> r) -> r
-    pub run: Rc<dyn 'a + for<'b> Fn(Rc<dyn Fn(A) -> R + 'b>) -> R>,
+    pub run: Rc<dyn Fn(Rc<dyn Fn(A) -> R + 'a>) -> R + 'a>,
 }
 
 impl<'a, R, A> Cont<'a, R, A> {
-    pub fn fmap<B: 'a, F>(&self, f: F) -> Cont<R, B>
+    pub fn fmap<B: 'a, F>(self, f: F) -> Cont<'a, R, B>
     where
-        F: Fn(A) -> B + 'a,
+        F: Fn(A) -> B + 'a + Clone,
+        A: 'a,
+        R: 'a,
     {
         //fmap f m = Cont $ \c -> run (c . f)
         //         = Cont (\c -> run (c . f))
         //         = Cont (\c -> run (\a -> c (f a)))
         Cont {
-            run: Rc::new(move |c: Rc<dyn Fn(B) -> R>| (self.run)(Rc::new(|a: A| c(f(a))))),
+            run: Rc::new(move |c: Rc<dyn Fn(B) -> R + 'a>| {
+                let f = f.clone();
+                (self.run)(Rc::new(move |a: A| c(f(a))))
+            }),
         }
     }
     pub fn pure(a: A) -> Cont<'a, R, A>
@@ -53,19 +59,21 @@ impl<'a, R, A> Cont<'a, R, A> {
     }
     pub fn bind<B, K>(self, k: K) -> Cont<'a, R, B>
     where
-        K: Fn(A) -> Cont<'a, R, B> + 'a,
+        K: Fn(A) -> Cont<'a, R, B> + 'a + Clone,
         A: 'a,
         R: 'a,
+        B: 'a,
     {
         Cont {
             run: Rc::new(move |c: Rc<dyn Fn(B) -> R>| {
-                (self.run)(Rc::new(|a: A| (k(a).run)(c.clone())))
+                let k = k.clone();
+                (self.run)(Rc::new(move |a: A| (k(a).run)(c.clone())))
             }),
         }
     }
     pub fn run_cont<F>(self, f: F) -> R
     where
-        F: Fn(A) -> R,
+        F: Fn(A) -> R + 'a,
     {
         (self.run)(Rc::new(move |a: A| f(a)))
     }
@@ -78,7 +86,7 @@ impl<'a, R> Cont<'a, R, R> {
 
 pub fn call_cc<'a, A, B, R, F>(f: F) -> Cont<'a, R, A>
 where
-    F: for<'b> Fn(Rc<dyn Fn(A) -> Cont<'b, R, B> + 'b>) -> Cont<'a, R, A> + 'a,
+    F: for<'b> Fn(Rc<dyn Fn(A) -> Cont<'a, R, B> + 'a>) -> Cont<'a, R, A> + 'a,
     R: 'a + Clone,
     A: 'a + Clone,
 {
@@ -87,9 +95,9 @@ where
     Cont { run: runit }
 }
 
-fn call_cc_outer<'a, A, B, R, F>(f: F) -> Rc<dyn for<'b> Fn(Rc<dyn Fn(A) -> R + 'b>) -> R + 'a>
+fn call_cc_outer<'a, A, B, R, F>(f: F) -> Rc<dyn for<'b> Fn(Rc<dyn Fn(A) -> R + 'a>) -> R + 'a>
 where
-    F: for<'b> Fn(Rc<dyn Fn(A) -> Cont<'b, R, B> + 'b>) -> Cont<'a, R, A> + 'a,
+    F: for<'b> Fn(Rc<dyn Fn(A) -> Cont<'a, R, B> + 'a>) -> Cont<'a, R, A> + 'a,
     A: Clone + 'a,
     R: 'a,
 {
