e20c33e0a0b92ed9d5fae7583d23488daf040aaa
Jason Dagit <dagitj@gmail.com>
2021-06-29 02:31:20 -0700

    Borrow ideas to implement a category theory stack of traits, make some instances

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use ghost::phantom;
 use std::rc::Rc;
 
 fn main() {
@@ -36,10 +37,18 @@ pub struct Cont<'a, R, A> {
 }
 
 impl<'a, R, A> Cont<'a, R, A> {
-    pub fn fmap<B: 'a, F>(self, f: F) -> Cont<'a, R, B>
+    ///cont :: ((a -> r) -> r) -> Cont r a
+    pub fn cont<F>(f: F) -> Cont<'a, R, A>
+    where
+        F: Fn(Rc<dyn Fn(A) -> R + 'a>) -> R + 'a,
+    {
+        Cont { run: Rc::new(f) }
+    }
+    pub fn fmap<B, F>(self, f: F) -> Cont<'a, R, B>
     where
         F: Fn(A) -> B + 'a + Clone,
         A: 'a,
+        B: 'a,
         R: 'a,
     {
         //fmap f m = Cont $ \c -> run (c . f)
@@ -121,4 +130,133 @@ where
             run: Rc::new(move |_| c(a.clone())),
         }
     })
+}
+
+// http://blog.sigfpe.com/2008/12/mother-of-all-monads.html
+// i x = cont (\fred -> x >>= fred)
+// where cont f = Cont { run: f }
+// i :: Monad m => m a -> Cont (m b) a
+pub fn lift<'a, A, B, M, F>(m: This<M, A>) -> Cont<'a, This<M, B>, A>
+where
+    M: Monad<'a, A, B> + 'a + Clone,
+    This<M, A>: Clone,
+    A: Clone + 'a,
+    B: Clone + 'a,
+{
+    Cont::cont(move |f| {
+        let m = m.clone();
+        M::bind(m, move |a: A| f(a))
+    })
+}
+
+// Heavily borrowed from here: https://github.com/RustyYato/type-families
+pub type This<T, A> = <T as Family<A>>::This;
+pub trait Family<A> {
+    type This;
+}
+
+pub trait Functor<'a, A, B>: Family<A> + Family<B> {
+    fn map<F>(f: F, this: This<Self, A>) -> This<Self, B>
+    where
+        F: Fn(A) -> B + 'a + Clone,
+        A: 'a,
+        B: 'a;
+}
+
+pub trait Pure<'a, A>: Family<A> {
+    fn pure(value: A) -> This<Self, A>
+    where
+        A: 'a + Clone;
+}
+
+pub trait Applicative<'a, A, B>: Functor<'a, A, B> + Pure<'a, A> + Pure<'a, B> {
+    fn apply<F>(a: This<Self, F>, b: This<Self, A>) -> This<Self, B>
+    where
+        F: Fn(A) -> B + 'a + Clone,
+        Self: Applicative<'a, F, A>,
+        A: 'a + Clone,
+        B: 'a + Clone,
+    {
+        Self::lift_a2(move |q: F, r| q(r), a, b)
+    }
+
+    fn lift_a2<C, F>(f: F, a: This<Self, A>, b: This<Self, B>) -> This<Self, C>
+    where
+        F: Fn(A, B) -> C + 'a + Clone,
+        Self: Pure<'a, C>,
+        Self: Family<C>,
+        A: 'a + Clone,
+        B: 'a + Clone,
+        C: 'a + Clone;
+}
+
+pub trait Monad<'a, A, B>: Applicative<'a, A, B> {
+    fn bind<F>(a: This<Self, A>, f: F) -> This<Self, B>
+    where
+        F: Fn(A) -> This<Self, B> + Clone + 'a;
+    fn compose<F, G, C>(f: F, g: G, a: A) -> This<Self, C>
+    where
+        F: FnOnce(A) -> This<Self, B>,
+        G: Fn(B) -> This<Self, C> + Clone + 'a,
+        Self: Monad<'a, B, C>,
+    {
+        Self::bind(f(a), g)
+    }
+}
+
+#[phantom]
+#[derive(Clone, Copy)]
+pub struct ContFamily<'a, R>;
+
+impl<'a, R: 'a, A: 'a> Family<A> for ContFamily<'a, R> {
+    type This = Cont<'a, R, A>;
+}
+
+impl<'a, R: 'a, A: 'a> Pure<'a, A> for ContFamily<'a, R> {
+    fn pure(value: A) -> This<Self, A>
+    where
+        A: Clone + 'a,
+    {
+        Cont::pure(value)
+    }
+}
+
+impl<'a, R: 'a, A: 'a, B: 'a> Functor<'a, A, B> for ContFamily<'a, R> {
+    fn map<F>(f: F, this: This<Self, A>) -> This<Self, B>
+    where
+        F: Fn(A) -> B + 'a + Clone,
+    {
+        this.fmap(f)
+    }
+}
+
+impl<'a, R: 'a + Clone, A: 'a + Clone, B: 'a + Clone> Applicative<'a, A, B> for ContFamily<'a, R> {
+    fn lift_a2<C, F>(f: F, a: This<Self, A>, b: This<Self, B>) -> This<Self, C>
+    where
+        F: Fn(A, B) -> C + 'a + Clone,
+        C: 'a + Clone,
+    {
+        a.bind(move |x: A| {
+            let f = f.clone();
+            let b = b.clone();
+            b.bind(move |y: B| {
+                let x = x.clone();
+                Cont::pure(f(x, y))
+            })
+        })
+    }
+}
+
+impl<'a, A, B, R> Monad<'a, A, B> for ContFamily<'a, R>
+where
+    R: Clone + 'a,
+    A: Clone + 'a,
+    B: Clone + 'a,
+{
+    fn bind<K>(a: This<Self, A>, k: K) -> This<Self, B>
+    where
+        K: Fn(A) -> Cont<'a, R, B> + 'a + Clone,
+    {
+        Cont::bind(a, k)
+    }
 }
